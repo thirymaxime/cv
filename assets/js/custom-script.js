ccfc16399b53e31739000a8e0fca27b43e797908
Thiry Maxime <thiry96.maxime@gmail.com>
2019-11-01 18:08:46 +0100

    ajout de la section présentation et avancement dans le style général

---
+++ assets/js/custom-script.js
@@ -0,0 +1,23 @@
+document.querySelector('div.logos-navbar > a:nth-child(1)').addEventListener('mouseover', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(1) > img').src = 'assets/img/github-logo-hover.png';
+});
+
+document.querySelector('div.logos-navbar > a:nth-child(1)').addEventListener('mouseout', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(1) > img').src = 'assets/img/github-logo.png';
+});
+
+document.querySelector('div.logos-navbar > a:nth-child(2)').addEventListener('mouseover', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(2) > img').src = 'assets/img/linkedin-logo-hover.png';
+});
+
+document.querySelector('div.logos-navbar > a:nth-child(2)').addEventListener('mouseout', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(2) > img').src = 'assets/img/linkedin-logo.png';
+});
+
+document.querySelector('div.logos-navbar > a:nth-child(3)').addEventListener('mouseover', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(3) > img').src = 'assets/img/email-logo-hover.png';
+});
+
+document.querySelector('div.logos-navbar > a:nth-child(3)').addEventListener('mouseout', function(){
+  document.querySelector('div.logos-navbar > a:nth-child(3) > img').src = 'assets/img/email-logo.png';
+});
