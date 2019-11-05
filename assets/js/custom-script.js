794ca747204ef7a3b8d8c8577f767990ded19ab2
Thiry Maxime <thiry96.maxime@gmail.com>
2019-11-05 23:50:09 +0100

    passage du JS en jquery et quelques modifs AOS

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,59 +1,25 @@
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
-
-$(function(){
+$(function init(){
+  AOS.init({disable: 'phone'});
   $('[data-toggle="github-tooltip"]').tooltip();
-});
-
-$(function(){
   $('[data-toggle="linkedin-tooltip"]').tooltip();
-});
-
-$(function(){
   $('[data-toggle="email-tooltip"]').tooltip();
-});
-
-$(function(){
   $('[data-toggle="link-top-tooltip"]').tooltip();
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(1)').addEventListener('mouseover', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(1) > img').src = 'assets/img/github-logo-hover.png';
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(1)').addEventListener('mouseout', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(1) > img').src = 'assets/img/github-logo.png';
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(2)').addEventListener('mouseover', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(2) > img').src = 'assets/img/linkedin-logo-hover.png';
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(2)').addEventListener('mouseout', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(2) > img').src = 'assets/img/linkedin-logo.png';
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(3)').addEventListener('mouseover', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(3) > img').src = 'assets/img/email-logo-hover.png';
-});
-
-document.querySelector('div.logos-navbar > a:nth-child(3)').addEventListener('mouseout', function(){
-  document.querySelector('div.logos-navbar > a:nth-child(3) > img').src = 'assets/img/email-logo.png';
+  $('div.logos-navbar > a:nth-child(1)').mouseover(function(){
+    $('div.logos-navbar > a:nth-child(1) > img').attr('src', 'assets/img/github-logo-hover.png');
+  });
+  $('div.logos-navbar > a:nth-child(1)').mouseout(function(){
+    $('div.logos-navbar > a:nth-child(1) > img').attr('src', 'assets/img/github-logo.png');
+  });
+  $('div.logos-navbar > a:nth-child(2)').mouseover(function(){
+    $('div.logos-navbar > a:nth-child(2) > img').attr('src', 'assets/img/linkedin-logo-hover.png');
+  });
+  $('div.logos-navbar > a:nth-child(2)').mouseout(function(){
+    $('div.logos-navbar > a:nth-child(2) > img').attr('src', 'assets/img/linkedin-logo.png');
+  });
+  $('div.logos-navbar > a:nth-child(3)').mouseover(function(){
+    $('div.logos-navbar > a:nth-child(3) > img').attr('src', 'assets/img/email-logo-hover.png');
+  });
+  $('div.logos-navbar > a:nth-child(3)').mouseout(function(){
+    $('div.logos-navbar > a:nth-child(3) > img').attr('src', 'assets/img/email-logo.png');
+  });
 });
